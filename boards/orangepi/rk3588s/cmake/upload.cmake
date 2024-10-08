e4445b1346548179409cbf90b33d420c0f2ac268
leeziy <lizy48@foxmail.com>
2024-10-08 18:43:04 +0800

    orangepi

--- boards/orangepi/rk3588s/cmake/upload.cmake
+++ boards/orangepi/rk3588s/cmake/upload.cmake
@@ -34,18 +34,18 @@
 if(DEFINED ENV{AUTOPILOT_HOST})
 	set(AUTOPILOT_HOST $ENV{AUTOPILOT_HOST})
 else()
-	set(AUTOPILOT_HOST "px4firefly")
+	set(AUTOPILOT_HOST "px4orangepi")
 endif()
 
 if(DEFINED ENV{AUTOPILOT_USER})
 	set(AUTOPILOT_USER $ENV{AUTOPILOT_USER})
 else()
-	set(AUTOPILOT_USER "firefly")
+	set(AUTOPILOT_USER "orangepi")
 endif()
 
 add_custom_target(upload
 	COMMAND rsync -arh --progress
-			${CMAKE_RUNTIME_OUTPUT_DIRECTORY} ${PX4_SOURCE_DIR}/posix-configs/firefly/rk3588s_mc.config ${PX4_BINARY_DIR}/etc # source
+			${CMAKE_RUNTIME_OUTPUT_DIRECTORY} ${PX4_SOURCE_DIR}/posix-configs/orangepi/rk3588s_mc.config ${PX4_BINARY_DIR}/etc # source
 			"${AUTOPILOT_USER}@${AUTOPILOT_HOST}:/home/${AUTOPILOT_USER}/px4" # destination
 	DEPENDS px4
 	COMMENT "uploading px4"
