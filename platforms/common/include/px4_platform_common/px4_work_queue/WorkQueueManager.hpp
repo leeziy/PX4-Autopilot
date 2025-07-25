5b1792553eb8ced5d9d3bdff1fc2b985726268f2
leeziy <lizy48@foxmail.com>
2025-07-25 13:38:46 +0800

    thread

--- platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
+++ platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
@@ -50,10 +50,10 @@ namespace wq_configurations
 {
 static constexpr wq_config_t rate_ctrl{"wq:rate_ctrl", 3150, 0}; // PX4 inner loop highest priority
 // Thread exclusive
-static constexpr wq_config_t control_allocator{"wq:control_allocator", 3150, 0};
-static constexpr wq_config_t mc_rate_control{"wq:mc_rate_control", 3150, 0};
+static constexpr wq_config_t control_allocator{"wq:ctl_alloc", 3150, 0};
+static constexpr wq_config_t mc_rate_control{"wq:mc_rate_ctl", 3150, 0};
 static constexpr wq_config_t pwm_out_sim{"wq:pwm_out_sim", 3150, 0};
-static constexpr wq_config_t vehicle_angular_velocity{"wq:vehicle_angular_velocity", 3150, 0};
+static constexpr wq_config_t vehicle_angular_velocity{"wq:vehicle_rate", 3150, 0};
 //
 
 static constexpr wq_config_t SPI0{"wq:SPI0", 2392, -1};
